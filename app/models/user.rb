20cc7cd957cde959ea4073d9644366d014ab575d
kazukiichimura <ikazuki@outlook.jp>
2015-08-26 08:14:12 +0000

    Make a Micropost model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ActiveRecord::Base
+   has_many :microposts, dependent: :destroy
    devise :database_authenticatable,:registerable,
           :recoverable, :rememberable, :trackable, :validatable
           
